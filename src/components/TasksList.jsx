a93add8248f722229c04a27855be79eaf12092ad
dalyyy3259@protonmail.com <dalyyy3259@protonmail.com>
2024-05-27 11:36:24 +0100

    style

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react";
 import Task from "./Task";
 
 function TasksList() {
-  const [tasks, setTasks] = useState([])
-  const [del,setDel] = useState(false)
+  const [tasks, setTasks] = useState([]);
+  const [del, setDel] = useState(false);
 
   useEffect(() => {
     setTasks(JSON?.parse(localStorage?.getItem("tasks")));
   }, [del]);
 
   return (
-    <div>
+    <div
+      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+    >
       {tasks?.map((e, i) => {
         return (
-          <div key={i}>
+          <div style={{ padding: "20px" }} key={i}>
             <Task del={del} setDel={setDel} task={e} index={i} />
           </div>
         );
